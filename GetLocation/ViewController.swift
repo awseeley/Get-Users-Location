b4a9155c040308f0227083f2b4fba51a04a0423a
Andrew Seeley <awseeley@gmail.com>
2017-04-17 14:56:48 +1000

    Updated Code
    Updated Code

--- GetLocation/ViewController.swift
+++ GetLocation/ViewController.swift
@@ -11,7 +11,7 @@ import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
-    
+    // Used to start getting the users location
     let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
@@ -24,25 +24,29 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         // For use when the app is open
         //locationManager.requestWhenInUseAuthorization()
         
+        // If location services is enabled get the users location
         if CLLocationManager.locationServicesEnabled() {
             locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest // You can change the locaiton accuary here.
             locationManager.startUpdatingLocation()
         }
     }
     
+    // Print out the location to the console
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.first {
             print(location.coordinate)
         }
     }
     
+    // If we have been deined access give the user the option to change it
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if(status == CLAuthorizationStatus.denied) {
             showLocationDisabledPopUp()
         }
     }
     
+    // Show the popup to the user if we have been deined access
     func showLocationDisabledPopUp() {
         let alertController = UIAlertController(title: "Background Location Access Disabled",
                                                 message: "In order to deliver pizza we need your location",
@@ -61,11 +65,5 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         self.present(alertController, animated: true, completion: nil)
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
